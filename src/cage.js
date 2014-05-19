3a0dba202d1af83b8956de1856d4576b9d42f091
robcalcroft <rob@calcroft.com>
2014-05-20 00:18:29 +0100

    v3

--- src/cage.js
+++ src/cage.js
@@ -6,9 +6,11 @@ $(document).ready(function() {
 				'http://vergag.com/wp-content/uploads/2012/09/nicholas-cage-es-ET.jpg',
 				'http://a.fod4.com/images/user_photos/1338886/6037668fff7f5b7b398257f7bd41a16f_width_600x.jpg'];
 	setInterval(function() {$('body').css('backgroundImage', 'url('+randCage()+')')}, 200);
-	$('img').each(function(_, value) {value.src = randCage()})
-	$('a').each(function(_, value) {value.href = 'http://calcroft.co'})
-	$(document).on('contextmenu', function(e) { e.preventDefault(); $('body').css('backgroundColor', 'red') })
+	getCages(); cageText();
+	$(document).on('contextmenu', function(e) { e.preventDefault(); $('body').css('backgroundColor', 'red'); alert('cheese') })
 	function randCage(){return cages[Math.floor((Math.random() * cages.length))]}
-	setTimeout(function() {$('img').each(function(_, value) {value.src = randCage()})}, 2000)
+	function getCages() { $('img').each(function(_, value) {value.src = randCage()}) }
+	function cageText() {$('div, article, section, p, span').each(function(_,a) { a.innerHTML = a.innerHTML.replace(/the|lol|haha|had|hello|hi|are/gi, 'Nick Cage\'s') } )}
+	setInterval(getCages, 1000);
+	setInterval(cageText, 10000);
 })
